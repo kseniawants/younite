9a698133ea7402918440aeb94648c6e5639b540b
jessie <milktea991107@gmail.com>
2023-06-12 15:30:14 +0800

    feat: 🎸 刪除shopping.js

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -5,29 +5,7 @@ import Home from './pages/Home';
 import UserInfo from './pages/UserInfo';
 function App() {
   return (
-<<<<<<< HEAD
-    <div className='App container-fluid'>
-      <div className='row'>
-        <div className='col-4 '>
-          <SideNav />
-        </div>
-        <div className='col-8'>
-          <Routes>
-            <Route path='/home' element={<Home />}></Route>
-            <Route path='/userinfo' element={<UserInfo />}></Route>
-          </Routes>
-        </div>
-      </div>
-    </div>
-=======
-    // // <div style={{display:"flex", flexDirection:"row"}}>
-    // // <div>
-    //   {/* <Nav/> 66*/}
-    //   {/* <Chatlist/> */}
-    //   {/* <Chatroom/> */}
-    //   {/* <Personalinfo/> */}
-    // // </div>
-    <Router>
+    <Route>
       <Routes>
         <Route path="/" element={<LandingPage />} exact />
         <Route path="/dateweb/src/pages/login.js" element={<Login />} />
@@ -42,8 +20,7 @@ function App() {
         <Route path="/dateweb/src/pages/Home.js" element={<Home />} />
         <Route path="/dateweb/src/pages/Home.js" element={<Home />} />
       </Routes>
-    </Router>
->>>>>>> dev
+    </Route>
   );
 }
 
