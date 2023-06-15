327f50617a6046a8e8c9f769699bed59e9c0f667
Aliang <lisa5566407@gmail.com>
2023-06-15 11:00:57 +0800

    feat: 🎸 nav 開發中

--- Front-End/src/pages/Home.js
+++ Front-End/src/pages/Home.js
@@ -6,36 +6,34 @@ import PeoplePhoto from '../component/User/PeoplePhoto';
 function Home() {
   return (
     <>
-      <div className='bg-page col-11'>
-        <div className='bg-pageMain'>
-          <div className='bg-pageTitle'>
-            <h5>你可能喜歡</h5>
-            <Button>
-              <i className='fa-solid fa-filter text-white'> 篩選</i>
-            </Button>
-          </div>
-          <div className='bg-pagePhoto'>
-            <PeoplePhoto />
-            <PeoplePhoto />
-            <PeoplePhoto />
-            <PeoplePhoto />
-            <PeoplePhoto />
-            <PeoplePhoto />
-          </div>
-          <div className='bg-pageMore'>
-            <a href=''>顯示更多...</a>
-          </div>
+      <div className='bg-pageMain'>
+        <div className='bg-pageTitle'>
+          <h5>你可能喜歡</h5>
+          <Button>
+            <i className='fa-solid fa-filter text-white'> 篩選</i>
+          </Button>
         </div>
-        <div className='homPageBottom'>
-          <div className='bg-pageBottomCard'>
-            <h6>你可能喜歡</h6>
-          </div>
-          <div className='bg-pageBottomCard'>
-            <h6>你可能喜歡</h6>
-          </div>
-          <div className='bg-pageBottomCard'>
-            <h6>你可能喜歡</h6>
-          </div>
+        <div className='bg-pagePhoto'>
+          <PeoplePhoto />
+          <PeoplePhoto />
+          <PeoplePhoto />
+          <PeoplePhoto />
+          <PeoplePhoto />
+          <PeoplePhoto />
+        </div>
+        <div className='bg-pageMore'>
+          <a href=''>顯示更多...</a>
+        </div>
+      </div>
+      <div className='homPageBottom'>
+        <div className='bg-pageBottomCard'>
+          <h6>你可能喜歡</h6>
+        </div>
+        <div className='bg-pageBottomCard'>
+          <h6>你可能喜歡</h6>
+        </div>
+        <div className='bg-pageBottomCard'>
+          <h6>你可能喜歡</h6>
         </div>
       </div>
     </>
--- Front-End/src/pages/UserInfo.js
+++ Front-End/src/pages/UserInfo.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import SideNav from '../component/SideNav';
+import { Button } from 'react-bootstrap';
+import '../styles/component/bdGradient.scss';
+import PeoplePhoto from '../component/User/PeoplePhoto';
 
 function UserInfo() {
   return (
     <>
-      <div className='container-fluid'>
-        <div className='row'>
-          <div className='col-2 p-0'>
-            <SideNav />
-          </div>
-          <div className='col-10'>
-            <h1>使用者資訊</h1>
-          </div>
+      <div className='bg-pageMain'>
+        <div className='bg-pageTitle'>
+          <h5>你可能喜歡</h5>
+          <Button>
+            <i className='fa-solid fa-filter text-white'> 篩選</i>
+          </Button>
+        </div>
+        <div className='bg-pagePhoto'>
+          <PeoplePhoto />
+          <PeoplePhoto />
+          <PeoplePhoto />
+          <PeoplePhoto />
+          <PeoplePhoto />
+          <PeoplePhoto />
+        </div>
+        <div className='bg-pageMore'>
+          <a href=''>顯示更多...</a>
+        </div>
+      </div>
+      <div className='homPageBottom'>
+        <div className='bg-pageBottomCard'>
+          <h6>你可能喜歡</h6>
+        </div>
+        <div className='bg-pageBottomCard'>
+          <h6>你可能喜歡</h6>
+        </div>
+        <div className='bg-pageBottomCard'>
+          <h6>你可能喜歡</h6>
         </div>
       </div>
     </>
