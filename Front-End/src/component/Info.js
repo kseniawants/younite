98820ce355169463927ac5a56b879f14c08655fe
jessie <milktea991107@gmail.com>
2023-06-20 09:34:44 +0800

    feat: 🎸 新增資料修改功能

--- Front-End/src/component/Info.js
+++ Front-End/src/component/Info.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import '../styles/all.scss';
-import { useForm } from 'react-hook-form';
-import { Input  } from './FormElements';
+import { useForm, Controller } from 'react-hook-form';
+import { Input, RadioButtonGroup } from './FormElements';
 import { DatePicker, Button, Divider } from 'antd';
-import { ButtonGroup, ToggleButton } from 'react-bootstrap';
-import Hobbies from './Hobbies';
+import Hobbies from './InfoElements/Hobbies';
+import Avatar from './InfoElements/Avatar';
+import PhotoWall from './InfoElements/PhotoWall';
 
 function Info() {
   const [submitting, setSubmitting] = useState(false);
     const {
       register,
       handleSubmit,
+      control,
       formState: { errors },
     } = useForm({
       mode: 'onTouched',
@@ -27,22 +29,22 @@ function Info() {
       }
     };
 
-    const onChange = (date, dateString) => {
-        console.log(date, dateString);
-    };
+    const radioGender = [
+      { label: '男生', value: 'male' },
+      { label: '女生', value: 'female' },
+      { label: '其他', value: 'other' }
+    ];
 
-    const [radioValue, setRadioValue] = useState('');
-    const [orientationValue, setOrientationValue] = useState('');
-    const [displayValue, setDisplayValue] = useState('');
-
-    const radios = [
-        { name: '男生', value: 'boy' },
-        { name: '女生', value: 'girl' },
-        { name: '其他', value: 'other' },
-        { name: '生理男', value: 'man' },
-        { name: '生理女', value: 'woman' },
-        { name: '雙性戀', value: 'bisexual' },
-        { name: '所有人', value: 'all' },
+    const radioSO = [
+      { label: '生理男', value: 'man' },
+      { label: '生理女', value: 'woman' },
+      { label: '雙性戀', value: 'bisexual' }
+    ];
+    
+    const radioShow = [
+      { label: '男生', value: 'men' },
+      { label: '女生', value: 'women' },
+      { label: '所有人', value: 'all' }
     ];
 
   return (
@@ -89,76 +91,64 @@ function Info() {
           <span className="text-danger p-1">*</span>
           <label htmlFor="">生日</label>
           <br />
-          <DatePicker onChange={onChange} placeholder="選擇生日" className='mt-2 shadow-sm'/>
+          <Controller
+          control={control}
+          name="birthday"
+          rules={{ required: true }}
+          render={({ field }) => (
+            <DatePicker
+              {...field}
+              placeholder="選擇生日"
+              className={`mt-2 shadow-sm ${errors.birthday && 'is-invalid'}`}
+            />
+          )}
+        />
+        {errors.birthday && <div className="invalid-feedback">請選擇生日</div>}
         </div>
         <div className='pb-4'>
           <i className="fa-solid fa-venus-mars text-black"></i>
           <span className="text-danger p-1">*</span>
           <label>性別</label>
           <br />
-          <ButtonGroup>
-            {radios.slice(0, 3).map((radio, idx) => (
-              <ToggleButton
-                key={idx}
-                id={`radio-gender-${idx}`}
-                type="radio"
-                variant={'outline-radio'}
-                name="radio-gender"
-                className='mt-2 me-4 px-4 rounded shadow-sm bg-white'
-                value={radio.value}
-                checked={radioValue === radio.value}
-                onChange={(e) => setRadioValue(e.currentTarget.value)}
-              >
-                {radio.name}
-              </ToggleButton>
-            ))}
-          </ButtonGroup>
+          <RadioButtonGroup
+            name="radioGender"
+            options={radioGender}
+            register={register}
+            errors={errors}
+            rules={{
+              required: '請選擇性別',
+            }}
+          />
         </div>
         <div className='pb-4'>
           <i className="fa-solid fa-transgender text-black"></i>
           <span className="text-danger p-1">*</span>
           <label>性向</label>
           <br />
-          <ButtonGroup>
-            {radios.slice(3, 6).map((radio, idx) => (
-              <ToggleButton
-                key={idx}
-                id={`radio-orientation-${idx}`}
-                type="radio"
-                variant={'outline-radio'}
-                name="radio-orientation"
-                className='mt-2 me-4 px-3 rounded shadow-sm bg-white'
-                value={radio.value}
-                checked={orientationValue === radio.value}
-                onChange={(e) => setOrientationValue(e.currentTarget.value)}
-              >
-                {radio.name}
-              </ToggleButton>
-            ))}
-          </ButtonGroup>
+          <RadioButtonGroup
+            name="radioSO"
+            options={radioSO}
+            register={register}
+            errors={errors}
+            rules={{
+              required: '請選擇性向',
+            }}
+          />
         </div>
         <div className='pb-4'>
           <i className="fa-solid fa-users text-black"></i>
           <span className="text-danger p-1">*</span>
           <label>顯示給我看</label>
           <br />
-          <ButtonGroup>
-            {radios.filter((radio, idx) => [0, 1, 6].includes(idx)).map((radio, idx) => (
-              <ToggleButton
-                key={idx}
-                id={`radio-display-${idx}`}
-                type="radio"
-                variant={'outline-radio'}
-                name="radio-display"
-                className='mt-2 me-4 px-4 rounded shadow-sm bg-white'
-                value={radio.value}
-                checked={displayValue === radio.value}
-                onChange={(e) => setDisplayValue(e.currentTarget.value)}
-              >
-                {radio.name}
-              </ToggleButton>
-            ))}
-          </ButtonGroup>
+          <RadioButtonGroup
+            name="radioShow"
+            options={radioShow}
+            register={register}
+            errors={errors}
+            rules={{
+              required: '顯示給我看為必填',
+            }}
+          />
         </div>
         <div className='pb-4'>
           <i className="fa-solid fa-heart-circle-plus text-black"></i>
@@ -173,18 +163,42 @@ function Info() {
             <span className="text-danger p-1">*</span>
             <label className='mb-2' >我想找尋</label>
             <br />
-            <Button type="dashed" >
-                +新增交友目標
-            </Button>
+            <Controller
+              control={control}
+              name="goal"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Button
+                  type="dashed"
+                  {...field}
+                  className={` ${errors.goal && 'is-invalid'}`}
+                >
+                    +新增交友目標
+                </Button>
+              )}
+            />
+            {errors.goal && <div className="invalid-feedback">請選擇交友目標</div>}
           </div>
-          <div className='mb-2 ps-5'>
+          <div className='mb-2 ps-4'>
             <i className="fa-solid fa-location-dot text-black"></i>
             <span className="text-danger p-1">*</span>
             <label className='mb-2'>出沒地點</label>
             <br />
-            <Button type="dashed" >
-                +地點
-            </Button>
+            <Controller
+              control={control}
+              name="address"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Button
+                  type="dashed"
+                  {...field}
+                  className={` ${errors.address && 'is-invalid'}`}
+                >
+                   +地點
+                </Button>
+              )}
+            />
+            {errors.address && <div className="invalid-feedback">請選擇地點</div>}
           </div>
         </div>
         <Divider plain style={{color: '#BBBBBB'}}>選填</Divider>
@@ -209,9 +223,11 @@ function Info() {
       <div className='col-md-5'>
         <div className='p-4 mb-4'>
           <label className='mb-4'>大頭貼照片</label>
+          <Avatar/>
         </div>
         <div className='p-4 mb-4'>
           <label className='mb-4'>個人檔案照片</label>
+          <PhotoWall/>
         </div>
       </div>
     </form>
---
+++ Front-End/src/component/InfoElements/Avatar.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import { Upload } from 'antd';
+import ImgCrop from 'antd-img-crop';
+
+const Avatar = () => {
+    const [fileList, setFileList] = useState([
+        {
+          uid: '-1',
+          name: 'image.png',
+          status: 'done',
+          url: 'https://images.unsplash.com/photo-1564485377539-4af72d1f6a2f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80',
+        },
+      ]);
+      const onChange = ({ fileList: newFileList }) => {
+        setFileList(newFileList);
+      };
+      const onPreview = async (file) => {
+        let src = file.url;
+        if (!src) {
+          src = await new Promise((resolve) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file.originFileObj);
+            reader.onload = () => resolve(reader.result);
+          });
+        }
+        const image = new Image();
+        image.src = src;
+        const imgWindow = window.open(src);
+        imgWindow?.document.write(image.outerHTML);
+      };
+
+  return (
+    <ImgCrop rotationSlider>
+      <Upload
+        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+        listType="picture-circle"
+        fileList={fileList}
+        onChange={onChange}
+        onPreview={onPreview}
+      >
+        {fileList.length < 5 && '+ Upload'}
+      </Upload>
+    </ImgCrop>
+  )
+}
+
+export default Avatar
