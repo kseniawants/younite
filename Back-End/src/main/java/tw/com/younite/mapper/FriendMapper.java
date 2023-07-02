75df0099d56d75895e693d40654befd2a5db03c7
David <qoqo850206@gmail.com>
2023-07-03 00:18:12 +0800

    feat: Create an API to manage invitation status, enabling automatic configuration of image, voice, and video functionalities.

--- Back-End/src/main/java/tw/com/younite/mapper/FriendMapper.java
+++ Back-End/src/main/java/tw/com/younite/mapper/FriendMapper.java
@@ -5,9 +5,16 @@ import java.util.List;
 
 public interface FriendMapper {
     Integer insertFriend(FriendEntity friendEntity);
-
     Integer setInvitationSent(FriendEntity friendEntity);
-
+    Integer setInvitationAccept(FriendEntity friendEntity);
+    Integer setInvitationStatus(FriendEntity friendEntity);
+    Integer setVideoChatStatus(FriendEntity friendEntity);
+    Integer setVoiceChatStatus(FriendEntity friendEntity);
+    Integer setImageStatus(FriendEntity friendEntity);
     List<FriendEntity> getFriend(Integer id);
     FriendEntity getSpecificFriend(Integer firstUserID, Integer secondUserID);
+    FriendEntity getInvitationStatus(Integer firstUserID,
+                                     Integer secondUserID,
+                                     Integer invitationStatus);
+
 }
