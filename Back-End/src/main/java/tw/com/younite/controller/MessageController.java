50ae671c0ef7c60c233346ffc40acf44f269ee92
saiyanfishh <raygun3491@icloud.com>
2023-07-11 12:13:35 +0800

    feat: MsgController+addMsg+getMsg

--- Back-End/src/main/java/tw/com/younite/controller/MessageController.java
+++ Back-End/src/main/java/tw/com/younite/controller/MessageController.java
@@ -1,6 +1,7 @@
 package tw.com.younite.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tw.com.younite.entity.MessageEntity;
 import tw.com.younite.service.impl.MessageService;
@@ -8,6 +9,7 @@ import tw.com.younite.service.inter.IMessageService;
 import tw.com.younite.util.JSONResult;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @RestController
 @RequestMapping("/message")
@@ -19,5 +21,10 @@ public class MessageController extends BaseController {
     public JSONResult<Void> addmessage(HttpSession session, @RequestBody MessageEntity messageEntity) {
         iMessageService.insertMessage(messageEntity);
         return new JSONResult<>(OK);
+    }
+    @GetMapping("/{roomId}")
+    public ResponseEntity<List<MessageEntity>> findMessage(HttpSession session,@PathVariable("roomId") Integer roomId){
+        List<MessageEntity> list= iMessageService.getMessages(roomId);
+        return ResponseEntity.ok(list);
     }
 }
