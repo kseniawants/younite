f32edf9aa1046ce7eb5e2962ac337da770ca3185
jessie <milktea991107@gmail.com>
2023-07-19 11:11:31 +0800

    update MailController

--- Back-End/src/main/java/tw/com/younite/controller/MailController.java
+++ Back-End/src/main/java/tw/com/younite/controller/MailController.java
@@ -13,6 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/mail")
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class MailController {
 
     @Autowired
